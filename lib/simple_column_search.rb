449968e93c4cf57b6d6882dbed66d369bdb3bee7
Elijah Miller <elijah.miller@gmail.com>
2008-12-23 19:52:16 -0500

    Plenty of doc cleanups, bumping version.

--- lib/simple_column_search.rb
+++ lib/simple_column_search.rb
@@ -1,19 +1,19 @@
 require 'rubygems'
 require 'activerecord'
 
-# Adds a Model.search('term1 term2') method that searches across SEARCH_COLUMNS
-# for OR'd terms using the LIKE operator with a pattern of "term1%".
-#
-# Usage:
-#   
-#   class User
-#     simple_column_search :email, :name
-#   end
-#  
-#   User.search('Firsty')
-#   User.search('Firsty Lasty')
-
 class ActiveRecord::Base
+  # Adds a Model.search('term1 term2') method that searches across SEARCH_COLUMNS
+  # for ANDed TERMS ORed across columns.
+  #   
+  #  class User
+  #    simple_column_search :first_name, :last_name
+  #  end
+  #  
+  #  User.search('elijah')          # => anyone with first or last name elijah
+  #  User.search('miller')          # => anyone with first or last name miller
+  #  User.search('elijah miller')
+  #    # => anyone with first or last name elijah AND
+  #    #    anyone with first or last name miller
   def self.simple_column_search(*columns)
     named_scope :search, lambda { |terms|
       conditions = terms.split.inject(nil) do |acc, term|
